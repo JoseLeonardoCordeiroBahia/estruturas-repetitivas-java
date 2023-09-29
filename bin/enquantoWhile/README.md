52ad3efa0fa753529426deb8ad8658117f45de00
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-09-29 10:02:15 -0300

    Estrutura repetitiva enquanto (while)

---
+++ bin/enquantoWhile/README.md
@@ -0,0 +1,5 @@
+## Estrutura repetitiva "enquanto"
+* Recomendada quando não se sabe previamente a quantidade de repetições
+* Regra
+    * Se a condição for VERDADEIRA executa e volta
+    * Se a condição for FALSA pula fora
---
+++ src/enquantoWhile/Exemplo1.java
@@ -0,0 +1,23 @@
+package enquantoWhile;
+
+import java.util.Scanner;
+
+public class Exemplo1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int soma = 0;
+		
+		while (x != 0) {
+			soma = soma + x;
+			x = scanner.nextInt();
+		}
+		
+		System.out.println(soma);
+		
+		scanner.close();
+	}
+	
+}
